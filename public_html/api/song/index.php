a2a644762440a1e5ed84b768335e70b1b188493f
Philip Thomsen <philip-th@live.dk>
2022-11-21 11:54:10 +0100

    Route::add create song

--- public_html/api/song/index.php
+++ public_html/api/song/index.php
@@ -15,5 +15,18 @@ Route::add('/api/song/([0-9]*)', function($id) {
     $result = $song->details($id);
     echo Tools::jsonParser($result);
 });
+
+Route::add('/api/song/', function() {
+    $song = new Song;
+    $song->title = isset($_POST['title']) && !empty($_POST['title']) ? $_POST['title'] : null;
+	$song->content = isset($_POST['content']) && !empty($_POST['content']) ? $_POST['content'] : null;
+	$song->artist_id = isset($_POST['artist_id']) && !empty($_POST['artist_id']) ? (int)$_POST['artist_id'] : null;
+
+    if ($song->title && $song->content && $song->artist_id) {
+        echo $song-create();
+    }else {
+        echo "Kan ikke oprette sang";
+    }
+}, 'post');
 Route::run('/');
 ?>
